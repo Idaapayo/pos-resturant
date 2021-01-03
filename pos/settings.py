b690e95dda8406488c08ee621ef7a886389c7895
unknown <idaapayo@gmail.com>
2021-01-03 17:57:20 +0300

    Basic sidebar functionality complete

--- pos/settings.py
+++ pos/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'u$1k=1wby9auf86r(d@%^=@@+_1g61m$syc=vxs0!o(!+j&s20'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.27']
+ALLOWED_HOSTS = [
+    '192.168.100.27',
+    '127.0.0.1'
+]
 
 
 # Application definition
