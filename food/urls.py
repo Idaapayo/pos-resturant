486d97aae5f1f1f65895694dc6294d2f4a332291
unknown <idaapayo@gmail.com>
2021-01-06 20:46:17 +0300

    starting listview for food page and some styling

--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from food.views import Sell_page
 
 urlpatterns = [
-    path('', views.food_home , name='food_home' ),
+    path('', views.food_home, name="food_home"),
+    path('', Sell_page.as_view() , name='sell_page' ),
     path('drinks', views.drinks, name='drinks')
 ]
--- food/views.py
+++ food/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from inventory.models import Products
 # Create your views here.
 
+class Sell_page(ListView):
+   model = Products
+   template_name = "food.html"
+
+   def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       return context
+
+
 def food_home(request):
-    
     return render(request, 'food.html')
 
 def drinks(request):
-
     return render(request, 'drinks.html')
